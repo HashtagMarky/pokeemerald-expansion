d9554c86286f51097bf7528f1950d9e521f6b722
surskitty <surskitty@gmail.com>
2021-01-15 14:52:43 -0500

    surskitty's Flying Taxi

--- include/constants/field_specials.h
+++ include/constants/field_specials.h
@@ -86,4 +86,7 @@
 #define DEOXYS_ROCK_SOLVED     2
 #define DEOXYS_ROCK_COMPLETE   3
 
+// Flying Taxi
+#define LAST_TALKED_TO_FLYING_TAXI 255 // used for determining if Fly or a flying taxi was used
+
 #endif // GUARD_CONSTANTS_FIELD_SPECIALS_H
